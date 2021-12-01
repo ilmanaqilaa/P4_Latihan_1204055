2426f1da11610dcf0abc582a34f8f2906684bdb0
ilmanaqilaa <ilmanaqilaa2@gmail.com>
2021-12-02 03:19:14 +0700

    Push latihan

---
+++ P4_4_1204055/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace P4_4_1204055
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHuruf = new System.Windows.Forms.TextBox();
+            this.txtNomor = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.epWarning = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epWrong = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epCorrect = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAplikasi = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtKota = new System.Windows.Forms.TextBox();
+            this.btSubmit = new System.Windows.Forms.Button();
+            this.btClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Form Pendaftaran Lomba Perancangan SIstem Aplikasi";
+            // 
+            // txtHuruf
+            // 
+            this.txtHuruf.Location = new System.Drawing.Point(201, 77);
+            this.txtHuruf.Name = "txtHuruf";
+            this.txtHuruf.Size = new System.Drawing.Size(100, 20);
+            this.txtHuruf.TabIndex = 1;
+            this.txtHuruf.Leave += new System.EventHandler(this.txtHuruf_Leave);
+            // 
+            // txtNomor
+            // 
+            this.txtNomor.Location = new System.Drawing.Point(201, 121);
+            this.txtNomor.Name = "txtNomor";
+            this.txtNomor.Size = new System.Drawing.Size(100, 20);
+            this.txtNomor.TabIndex = 2;
+            this.txtNomor.Leave += new System.EventHandler(this.txtNomor_Leave);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(201, 165);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 3;
+            this.txtEmail.Leave += new System.EventHandler(this.txtEmail_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "No. Handphone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Email";
+            // 
+            // epWarning
+            // 
+            this.epWarning.ContainerControl = this;
+            this.epWarning.Icon = ((System.Drawing.Icon)(resources.GetObject("epWarning.Icon")));
+            // 
+            // epWrong
+            // 
+            this.epWrong.ContainerControl = this;
+            this.epWrong.Icon = ((System.Drawing.Icon)(resources.GetObject("epWrong.Icon")));
+            // 
+            // epCorrect
+            // 
+            this.epCorrect.ContainerControl = this;
+            this.epCorrect.Icon = ((System.Drawing.Icon)(resources.GetObject("epCorrect.Icon")));
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Nama Aplikasi";
+            // 
+            // txtAplikasi
+            // 
+            this.txtAplikasi.Location = new System.Drawing.Point(201, 209);
+            this.txtAplikasi.Name = "txtAplikasi";
+            this.txtAplikasi.Size = new System.Drawing.Size(100, 20);
+            this.txtAplikasi.TabIndex = 8;
+            this.txtAplikasi.Leave += new System.EventHandler(this.txtAplikasi_Leave);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 256);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Kota";
+            // 
+            // txtKota
+            // 
+            this.txtKota.Location = new System.Drawing.Point(201, 253);
+            this.txtKota.Name = "txtKota";
+            this.txtKota.Size = new System.Drawing.Size(100, 20);
+            this.txtKota.TabIndex = 10;
+            this.txtKota.Leave += new System.EventHandler(this.txtKota_Leave);
+            // 
+            // btSubmit
+            // 
+            this.btSubmit.Location = new System.Drawing.Point(51, 364);
+            this.btSubmit.Name = "btSubmit";
+            this.btSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btSubmit.TabIndex = 11;
+            this.btSubmit.Text = "Submit";
+            this.btSubmit.UseVisualStyleBackColor = true;
+            this.btSubmit.Click += new System.EventHandler(this.btSubmit_Click);
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(226, 364);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(75, 23);
+            this.btClose.TabIndex = 12;
+            this.btClose.Text = "Close";
+            this.btClose.UseVisualStyleBackColor = true;
+            this.btClose.Click += new System.EventHandler(this.btClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 456);
+            this.Controls.Add(this.btClose);
+            this.Controls.Add(this.btSubmit);
+            this.Controls.Add(this.txtKota);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtAplikasi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNomor);
+            this.Controls.Add(this.txtHuruf);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHuruf;
+        private System.Windows.Forms.TextBox txtNomor;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ErrorProvider epWarning;
+        private System.Windows.Forms.ErrorProvider epWrong;
+        private System.Windows.Forms.ErrorProvider epCorrect;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAplikasi;
+        private System.Windows.Forms.TextBox txtKota;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btClose;
+        private System.Windows.Forms.Button btSubmit;
+    }
+}
+
---
+++ P4_4_1204055/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace P4_4_1204055
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtHuruf_Leave(object sender, EventArgs e)
+        {
+            if (txtHuruf.Text == "")
+            {
+                epWarning.SetError(txtHuruf, "Textbox Huruf tidak boleh kosong!");
+                epWrong.SetError(txtHuruf, "");
+                epCorrect.SetError(txtHuruf, "");
+            }
+            else
+            {
+                if ((txtHuruf.Text).Any(Char.IsLetter))
+                {
+                    epWarning.SetError(txtHuruf, "");
+                    epWrong.SetError(txtHuruf, "");
+                    epCorrect.SetError(txtHuruf, "Betul!");
+                }
+                else
+                {
+                    epWrong.SetError(txtHuruf, "Inputan hanya boleh huruf!");
+                    epWarning.SetError(txtHuruf, "");
+                    epCorrect.SetError(txtHuruf, "");
+                }
+            }
+        }
+
+        private void txtNomor_Leave(object sender, EventArgs e)
+        {
+            if(txtNomor.Text.Length > 12)
+            {
+                epWarning.SetError(txtNomor, "");
+                epWrong.SetError(txtNomor, "No telfon tidak boleh lebih dari 12 digit ya!");
+                epCorrect.SetError(txtNomor, "");
+            }
+            else if (txtNomor.Text == "")
+            {
+                epWarning.SetError(txtNomor, "Textbox Angka tidak boleh kosong!");
+                epWrong.SetError(txtNomor, "");
+                epCorrect.SetError(txtNomor, "");
+            }
+            else
+            {
+                if ((txtNomor.Text).All(Char.IsNumber))
+                {
+                    epCorrect.SetError(txtNomor, "Betul!");
+                    epWarning.SetError(txtNomor, "");
+                    epWrong.SetError(txtNomor, "");
+
+                }
+                else
+                {
+
+                    epWarning.SetError(txtNomor, "");
+                    epCorrect.SetError(txtNomor, "");
+                    epWrong.SetError(txtNomor, "Inputan hanya boleh huruf!");
+                }
+            }
+        }
+
+        private void txtEmail_Leave(object sender, EventArgs e)
+        {
+            if (txtEmail.Text == "")
+            {
+                epWarning.SetError(txtEmail, "Textbox Email tidak boleh kosong!");
+                epWrong.SetError(txtEmail, "");
+                epCorrect.SetError(txtEmail, "");
+            }
+            else
+            {
+                if (Regex.IsMatch(txtEmail.Text, @"^^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+                {
+                    epWarning.SetError(txtEmail, "");
+                    epWrong.SetError(txtEmail, "");
+                    epCorrect.SetError(txtEmail, "Betul!");
+
+                }
+                else
+                {
+                    epWarning.SetError(txtEmail, "");
+                    epCorrect.SetError(txtEmail, "");
+                    epWrong.SetError(txtEmail, "Format email salah!\nContoh: a@b.c");
+                }
+            }
+        }
+
+
+
+        private void txtAplikasi_Leave(object sender, EventArgs e)
+        {
+            if (txtAplikasi.Text == "")
+            {
+                epWarning.SetError(txtAplikasi, "Textbox Huruf tidak boleh kosong!");
+                epWrong.SetError(txtAplikasi, "");
+                epCorrect.SetError(txtAplikasi, "");
+            }
+            else
+            {
+                if (txtAplikasi.Text != txtAplikasi.Text.ToLower())
+                {
+                    epWarning.SetError(txtAplikasi, "");
+                    epWrong.SetError(txtAplikasi, "Nama Aplikasi diharapkan menggunakan huruf kecil ya!");
+                    epCorrect.SetError(txtAplikasi, "");
+                }
+                else if ((txtAplikasi.Text).Any(Char.IsLetter))
+                {
+                    epWarning.SetError(txtAplikasi, "");
+                    epWrong.SetError(txtAplikasi, "");
+                    epCorrect.SetError(txtAplikasi, "Betul!");
+                }
+
+                else
+                {
+                    epWrong.SetError(txtAplikasi, "Inputan hanya boleh huruf!");
+                    epWarning.SetError(txtAplikasi, "");
+                    epCorrect.SetError(txtAplikasi, "");
+                }
+            }
+        }
+
+
+
+        private void txtKota_Leave(object sender, EventArgs e)
+        {
+            if (txtKota.Text == "")
+            {
+                epWarning.SetError(txtKota, "Textbox Huruf tidak boleh kosong!");
+                epWrong.SetError(txtKota, "");
+                epCorrect.SetError(txtKota, "");
+            }
+            else
+            {
+                if (txtKota.Text != txtKota.Text.ToUpper())
+                {
+                    epWarning.SetError(txtKota, "");
+                    epWrong.SetError(txtKota, "Nama Aplikasi diharapkan menggunakan huruf kapital ya!");
+                    epCorrect.SetError(txtKota, "");
+                }
+                else if ((txtKota.Text).Any(Char.IsLetter))
+                {
+                    epWarning.SetError(txtKota, "");
+                    epWrong.SetError(txtKota, "");
+                    epCorrect.SetError(txtKota, "Betul!");
+                }
+
+                else
+                {
+                    epWrong.SetError(txtKota, "Inputan hanya boleh huruf!");
+                    epWarning.SetError(txtKota, "");
+                    epCorrect.SetError(txtKota, "");
+                }
+            }
+        }
+
+        private void btSubmit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show
+                   ("Nama : " + txtHuruf.Text +
+                   "\nNomor Handphone    : " + txtNomor.Text +
+                   "\nEmail    : " + txtEmail.Text +
+                   "\nNama Aplikasi: " + txtAplikasi.Text +
+                   "\nTujuan Aplikasi: " + txtKota.Text,
+                   "Informasi Pendaftaran",
+                   MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+    }
+
